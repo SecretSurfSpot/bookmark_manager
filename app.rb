c01b21be3528f183844c2d4fd1d58b5267f4d7e1
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-28 14:25:51 +0100

    add basic structure and app.rb

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+
+class BookmarkManager < Sinatra::Base
+
+  get '/' do
+    "Hello world"
+  end
+
+
+  # Required if running from command line (ruby app.rb) and not using 'rackup'
+  run! if app_file == $0
+
+end
