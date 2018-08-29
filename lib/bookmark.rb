4e74577e7b90b569a4dba4c2f9a7cf75e6e47b53
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-29 15:21:48 +0100

    Modify code in Bookmark #self.all to connect and query DB

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,11 +1,24 @@
+require 'pg'
+
 class Bookmark
 
   def self.all
-    [
-      'http://google.com',
-      'http://bbc.co.uk',
-      'http://facebook.com'
-    ]
+    # Code created by Mathilde and myself - replace by Code
+    # from walkthrough
+    # connection = PG.connect(dbname: 'bookmark_manager')
+    # bookmarks = connection.exec "select url from bookmarks"
+    # bookmarks.each do |row|
+    #   row.each do |key, value|
+    #     puts "#{value}"
+    #   end
+    # end
+
+    # Code from walkthrough
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks')
+    result.map { |bookmark| bookmark['url'] }
+
+
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,6 +4,7 @@ describe Bookmark do
     it  "displays a list of all bookmarks" do
       bookmarks = Bookmark.all
 
+      expect(bookmarks).to match_array(['http://google.com','http://bbc.co.uk','http://facebook.com'])
       expect(bookmarks).to include('http://google.com')
       expect(bookmarks).to include('http://bbc.co.uk')
       expect(bookmarks).to include('http://facebook.com')
